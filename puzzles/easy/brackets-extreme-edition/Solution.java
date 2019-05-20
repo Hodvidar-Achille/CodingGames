750fc5419fd9d44d36b6df4a337868b71393daa8
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-05-20 15:14:54 +0200

    Create Solution.java

---
+++ puzzles/easy/brackets-extreme-edition/Solution.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ *    https://www.codingame.com/ide/puzzle/brackets-extreme-edition
+ * By Hodvidar
+ **/
+class Solution {
+
+    private static final char PARE_OPEN = '(';
+    private static final char CROC_OPEN = '[';
+    private static final char ACCO_OPEN = '{';
+    private static final char PARE_CLOSE = ')';
+    private static final char CROC_CLOSE = ']';
+    private static final char ACCO_CLOSE = '}';
+    
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String expression = in.next();
+        
+        System.err.println("expression: "+expression);
+
+        int numberOfPareOpenned = 0;
+        int numberOfCrocOpenned = 0;
+        int numberOfAccoOpenned = 0;
+        
+        boolean expressionOK = true;
+        
+        for(char c : expression.toCharArray())
+        {
+            switch(c) {
+                case PARE_OPEN:
+                    numberOfPareOpenned++;
+                    break;
+                case CROC_OPEN:
+                   numberOfCrocOpenned++;
+                    break;
+                case ACCO_OPEN:
+                    numberOfAccoOpenned++;
+                    break;
+                case PARE_CLOSE:
+                    numberOfPareOpenned--;
+                    break;
+                case CROC_CLOSE:
+                    numberOfCrocOpenned--;
+                    break;
+                case ACCO_CLOSE:
+                    numberOfAccoOpenned--;
+                    break;
+                default:
+                    // code block
+            }
+            
+            if(numberOfPareOpenned < 0 || numberOfCrocOpenned < 0 || numberOfAccoOpenned < 0)
+                break;
+        }
+
+         if(numberOfPareOpenned != 0 || numberOfCrocOpenned != 0 || numberOfAccoOpenned != 0)
+             expressionOK = false;
+        
+        System.out.println(expressionOK);
+    }
+}
