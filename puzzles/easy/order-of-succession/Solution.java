f5ec6ff015f7a750bb9a5e0b200db57cbd6e29aa
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-05-20 18:24:00 +0200

    Create Solution.java

---
+++ puzzles/easy/order-of-succession/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            String parent = in.next();
+            int birth = in.nextInt();
+            String death = in.next();
+            String religion = in.next();
+            String gender = in.next();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("orDeroFsucceSsion");
+    }
+}
