940b237d714bf02ee6a40d440329d5b598035f04
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-05-27 00:49:11 +0200

    Create Solution.java

---
+++ puzzles/easy/may-the-triforce-be-with-you/Solution.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    private static final char asterix = '*';
+    private static final char space = ' ';
+    
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+        
+        // 1) Find the distance between the first dot '.' and the first start '*'
+        int distance = 
+        
+        // 2) Build the first triforce
+        // 3) build the 2 others triforces
+        
+
+        System.out.println("answer");
+    }
+    
+    private int getNextSize
+}
+
+
+
+.        *
+        ***
+       *****
+      *******
+     *********
+    *         *
+   ***       ***
+  *****     *****
+ *******   *******
+********* *********
