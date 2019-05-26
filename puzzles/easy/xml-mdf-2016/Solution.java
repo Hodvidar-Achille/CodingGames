181f15aeef774ab77bd90f5ad47e0cc6468e92a2
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-05-27 00:37:48 +0200

    Update Solution.java

--- puzzles/easy/xml-mdf-2016/Solution.java
+++ puzzles/easy/xml-mdf-2016/Solution.java
@@ -3,10 +3,8 @@ import java.io.*;
 import java.math.*;
 
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- 
- TODO : to continue
+ *    https://www.codingame.com/ide/puzzle/xml-mdf-2016
+ * by Hodvidar
  **/
 class Solution {
 
@@ -15,30 +13,112 @@ class Solution {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         String sequence = in.nextLine();
+        System.err.println("sequence:"+sequence);
         
-        Map<Character, Integer> weights = new HashMap<>();
-        
+        Set<Character> letters = new HashSet<>();
+        TagHolder fisrtTag = new TagHolder();
+        Tag currentHolder = fisrtTag;
+        boolean nextClose = false;
         for(char c : sequence.toCharArray())
         {
-            Integer w = weigths.get(c);
-            if(w == null)
+            if(c == END_TAG)
             {
-                weigths.put(c, Integer.valueOf(0));
+                nextClose = true;
+                continue;
             }
-            
+            if(nextClose)
+            {
+                currentHolder = currentHolder.parent;
+                nextClose = false;
+                continue;
+            }
+            letters.add(c);
+            Tag newTag = new Tag(c, currentHolder);
+            currentHolder = newTag;
         }
 
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
+        
+        char max = fisrtTag.getHeavierTag(letters);
 
-        System.out.println("answer");
+        System.out.println(max);
     }
 }
 
 class Tag
 {
-    public final char name;
-    public final int depth;
-    public final List<Tag> children = new ArrayList<>();
+    private final char name;
+    private final double depth;
+    private final List<Tag> children = new ArrayList<>();
+    public final Tag parent;
     
+    public Tag(char name, Tag parent)
+    {
+        this.name = name;
+        this.parent = parent;
+        if(this.parent == null)
+        {
+            this.depth = 0;
+        }
+        else
+        {
+            this.depth = 1 + this.parent.depth;
+            this.parent.children.add(this);
+        }
+    }
+    
+    // recursive
+    protected double getWeight(char aName)
+    {
+        // System.err.println("Tag("+this.name+")#getWeight("+aName+")");
+        double totalWeight = 0;
+        for(Tag child : this.children)
+        {
+            if(aName == child.name)
+                totalWeight += 1/child.depth;
+            totalWeight += child.getWeight(aName);
+        }
+        // System.err.println("totalWeight="+totalWeight);
+        return totalWeight;
+    }
+}
+
+class TagHolder extends Tag
+{
+    private final Set<Character> letters = new HashSet<>();
+    
+    public TagHolder()
+    {
+        super('_', null);
+    }
+    
+    public char getHeavierTag(Set<Character> tags)
+    {
+        System.err.println("getHeavierTag...:");
+        char heavier = '_';
+        double maxWeight = 0;
+        for(Character c : tags)
+        {
+            double weight = this.getWeight(c);
+            System.err.println("c: "+c+"  weight="+weight);
+            
+            if(weight < maxWeight)
+                continue;
+                
+            if(weight > maxWeight)
+            {
+                maxWeight = weight;
+                heavier = c;
+                continue;
+            }
+            
+            if(c < heavier)
+            {
+                maxWeight = weight;
+                heavier = c;
+            }
+        }
+        return heavier;
+    }
 }
