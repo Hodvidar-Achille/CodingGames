0da2c55c6197db4db8aca64730db37de120dbdbc
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-06-14 11:06:51 +0200

    Create Solution.java

---
+++ puzzles/easy/blowing-fuse/Solution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ *    https://www.codingame.com/ide/puzzle/blowing-fuse
+ * by Hodvidar
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int numberOfAppliances = in.nextInt();
+        int numberOfClicks = in.nextInt();
+        int mainFuseCapacity = in.nextInt();
+        
+        int[] appliancesConsumption = new int[numberOfAppliances];
+        for (int i = 0; i < numberOfAppliances; i++) {
+            int electricalConsumption = in.nextInt();
+            appliancesConsumption[i] = electricalConsumption;
+        }
+        
+        int maxElectricalConsumption = 0;
+        int[] appliancesState = new int[numberOfAppliances];
+        
+        for (int i = 0; i < numberOfClicks; i++) {
+            int applianceUsed = in.nextInt();
+            int id = applianceUsed - 1;
+            
+            // light On or light off the applicance
+            if(appliancesState[id] == 0)
+            	appliancesState[id] = appliancesConsumption[id];
+            else
+            	appliancesState[id] = 0;
+            	
+            
+            // calculate the total
+            int currentTotalConsumption = Arrays.stream(appliancesState).sum();
+            
+            if(currentTotalConsumption > mainFuseCapacity)
+            {
+            	System.out.println("Fuse was blown.");
+            	return;
+            }
+            if(currentTotalConsumption > maxElectricalConsumption)
+            {
+            	maxElectricalConsumption = currentTotalConsumption;
+            }
+        }
+        
+        
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("Fuse was not blown.");
+        System.out.println("Maximal consumed current was "+maxElectricalConsumption+" A.");
+    }
+}
