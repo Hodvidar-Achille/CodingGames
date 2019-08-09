92ca3116126d9b07f70faba8ebebcad7262da511
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2019-08-09 18:22:07 +0200

    Create Solution.java

---
+++ puzzles/hard/winamax-sponsored-contest/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int width = in.nextInt();
+        int height = in.nextInt();
+        for (int i = 0; i < height; i++) {
+            String row = in.next();
+        }
+        
+        // 1) 
+    }
+}
