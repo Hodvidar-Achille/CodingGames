90616e3452083f0f92292f64eb28a228376d233a
Hodvidar <44214409+Hodvidarr@users.noreply.github.com>
2020-07-30 09:40:29 +0200

    Create spreadsheet1D.py

---
+++ src/com/hodvidarr/codingame/puzzles/easy/spreadsheet1D.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+VALUE = "VALUE"
+ADD = "ADD"
+SUB = "SUB"
+MULT = "MULT"
+n = int(input())
+for i in range(n):
+    operation, arg_1, arg_2 = input().split()
+for i in range(n):
+    print("Debug messages...", file=sys.stderr, flush=True)
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    print("1")
