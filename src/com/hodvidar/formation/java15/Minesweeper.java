1bf44638f36e7f4ea26bd19ae42008ca54ce689e
@a.genet <a.genet@oodrive.com>
2020-09-23 12:06:33 +0200

    Continue Kata for Minesweeper, with java level 15.

--- src/com/hodvidar/formation/java15/Minesweeper.java
+++ src/com/hodvidar/formation/java15/Minesweeper.java
@@ -1,18 +1,21 @@
 package com.hodvidar.formation.java15;
 
+import static java.lang.Integer.parseInt;
+
+
 // Minesweeper
 public class Minesweeper {
 
 	public static String computeHintMap(String grid) {
-		if(grid.contains("2")) {
-			return
-					"""
-					00
-					""";
-		}
-		return
-    		"""
-			0
-			""";
+		var gridIterator = grid.lines().iterator();
+		var firstline = gridIterator.next();
+		var rows = firstline.split(" ")[0];
+		var columns = firstline.split(" ")[1];
+		var isMine = grid.contains("*");
+		var v = (isMine) ? "*" : "0";
+		var hintMap = v.repeat(parseInt(columns));
+		hintMap = (hintMap+"\n").repeat(parseInt(rows));
+
+		return hintMap;
 	}
 }
--- srcTest/com/hodvidar/formation/java15/MinesweeperTest.java
+++ srcTest/com/hodvidar/formation/java15/MinesweeperTest.java
@@ -4,47 +4,127 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-
+// https://codingdojo.org/kata/Minesweeper/
+/*
+ * *...
+ * ....
+ * .*..
+ * ....
+ * -->
+ * *100
+ * 2210
+ * 1*10
+ * 1110
+ */
 public class MinesweeperTest {
 
 	@Test
-	void should_compute_a_1_on_1_grid_without_mine() {
-		var grid = """
+	void should_compute_a_1_on_1_grid_without_landmine() {
+		checkHintMap("""
  				1 1
  				.
- 				""";
-		var hintMap = Minesweeper.computeHintMap(grid);
-		assertThat(hintMap).isEqualTo(
-    			"""
-				0
-				""");
+ 				""", """
+				 0
+				 """);
 	}
 
 	@Test
-	void should_compute_a_1_on_2_grid_without_mine() {
-		var grid = """
+	void should_compute_a_1_on_2_grid_without_landmine() {
+		checkHintMap("""
  				1 2
  				..
- 				""";
-		var hintMap = Minesweeper.computeHintMap(grid);
-		assertThat(hintMap).isEqualTo(
-				"""
-				00
-				""");
+ 				""", """
+				 00
+				 """);
 	}
 
 	@Test
-	void should_compute_a_2_on_2_grid_without_mine() {
-		var grid = """
+	void should_compute_a_1_on_3_grid_without_landmine() {
+		checkHintMap("""
+ 				1 3
+ 				...
+ 				""", """
+				 000
+				 """);
+	}
+
+	@Test
+	void should_compute_a_2_on_2_grid_without_landmine() {
+		checkHintMap("""
  				2 2
  				..
  				..
- 				""";
-		var hintMap = Minesweeper.computeHintMap(grid);
-		assertThat(hintMap).isEqualTo(
-				"""
-				00
-				00
+ 				""", """
+				 00
+				 00
+				 """);
+	}
+
+	@Test
+	void should_compute_a_5_on_4_grid_without_landmine() {
+		checkHintMap("""
+ 				5 4
+ 				....
+ 				....
+ 				....
+ 				....
+ 				....
+ 				""", """
+				 0000
+				 0000
+				 0000
+				 0000
+				 0000
+				 """);
+	}
+
+	@Test
+	void should_compute_a_1_on_1_grid_with_1_landmine() {
+		checkHintMap("""
+				1 1
+				*
+				""", """
+				*
 				""");
 	}
+
+	@Test
+	void should_compute_a_1_on_2_grid_with_2_landmines() {
+		checkHintMap("""
+				1 2
+				**
+				""", """
+				**
+				""");
+	}
+
+	@Test
+	void should_compute_a_2_on_2_grid_with_4_landmines() {
+		checkHintMap("""
+				2 2
+				**
+				**
+				""",
+    """
+				**
+				**
+				""");
+	}
+
+	@Test
+	void should_compute_a_1_on_2_grid_with_1_landmine1() {
+		checkHintMap("""
+				1 2
+				*.
+				""", """
+				*1
+				""");
+	}
+
+	private static void checkHintMap(String grid, String expectedHintMap) {
+		var hintMap = Minesweeper.computeHintMap(grid);
+		assertThat(hintMap).isEqualTo(
+				expectedHintMap);
+	}
+
 }
