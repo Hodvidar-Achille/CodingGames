0d958138f725f69c50744718c7ffb6867d72ac36
Hodvidar-Achille <Hodvidar@protonmail.com>
2021-05-19 13:41:17 +0200

    Clean up code

--- src/main/java/com/hodvidar/leetcode/medium/ThreeSum.java
+++ src/main/java/com/hodvidar/leetcode/medium/ThreeSum.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
  * <p>
  * Start: 12:30
  * Finished: 12:57 OK but too slow.
- * Finished:
+ * Finished: 13:30 done but read solution.
  */
 public class ThreeSum {
 
@@ -56,25 +56,24 @@ public class ThreeSum {
         return new ArrayList<>(solutions.values());
     }
 
-    public static List<List<Integer>> threeSumFaster2(int[] nums) {
+    public static List<List<Integer>> threeSumFaster2(final int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> li = new LinkedList<>();
-        for(int i=0; i< nums.length-2; i++){
-            if(i ==0 || (i>0 && nums[i] != nums[i-1])){
-                int low = i+1;
-                int high = nums.length-1;
-                int sum = 0-nums[i];
-                while(low<high){
-                    if(nums[low] + nums[high] == sum){
+        final List<List<Integer>> li = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                final int sum = 0 - nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
                         li.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        while(low < high && nums[low] == nums[low+1]) low ++;
-                        while(low< high && nums[high] == nums[high-1]) high--;
+                        while (low < high && nums[low] == nums[low + 1]) low++;
+                        while (low < high && nums[high] == nums[high - 1]) high--;
                         low++;
                         high--;
-                    } else if(nums[low] + nums[high] > sum){
+                    } else if (nums[low] + nums[high] > sum) {
                         high--;
-                    }
-                    else{
+                    } else {
                         low++;
                     }
                 }
