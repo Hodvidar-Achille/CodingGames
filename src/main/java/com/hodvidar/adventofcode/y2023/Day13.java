1fe8742ede5e565f0b309c129c82bf5b861516a9
Hodvidar-Achille <hodvidar@protonmail.com>
2023-12-14 00:56:47 +0100

    AdventOfCode2023 Day13 p1

---
+++ src/main/java/com/hodvidar/adventofcode/y2023/Day13.java
@@ -0,0 +1,106 @@
+package com.hodvidar.adventofcode.y2023;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day13 extends AbstractAdventOfCode2023 {
+
+    private ValleyOfMirrors valleyOfMirrors;
+
+    @Override
+    public int getDay() {
+        return 13;
+    }
+
+    @Override
+    public double getResultDouble(final Scanner sc) {
+        valleyOfMirrors = new ValleyOfMirrors();
+        while (sc.hasNext()) {
+            digestLine(sc.nextLine());
+        }
+        return valleyOfMirrors.getSummarizedSum();
+    }
+
+    @Override
+    protected void digestLine(final String line) {
+        valleyOfMirrors.addNewLinePattern(line);
+    }
+
+    private static class ValleyOfMirrors {
+        private final List<ValleyPattern> patterns = new ArrayList<>();
+
+        private int currentPatternIndex = 0;
+
+        public ValleyOfMirrors() {
+            patterns.add(new ValleyPattern());
+        }
+
+        public void addNewLinePattern(final String line) {
+            if (line.isBlank()) {
+                patterns.add(new ValleyPattern());
+                currentPatternIndex++;
+                return;
+            }
+            patterns.get(currentPatternIndex).addHorizontalLine(line);
+        }
+
+        public int getSummarizedSum() {
+            return patterns.stream().mapToInt(ValleyPattern::getSummarizedMirrorPosition).sum();
+        }
+    }
+
+    private static class ValleyPattern {
+        private final List<String> horizontalLines = new ArrayList<>();
+        private final List<String> verticalLines = new ArrayList<>();
+
+        public void addHorizontalLine(final String line) {
+            horizontalLines.add(line);
+        }
+
+        public int getSummarizedMirrorPosition() {
+            int position = getPositionFromLines(horizontalLines);
+            if (position != -1) {
+                return position * 100;
+            }
+            if (verticalLines.isEmpty()) {
+                buildVerticalLines();
+            }
+            position = getPositionFromLines(verticalLines);
+            return position;
+        }
+
+        private void buildVerticalLines() {
+            final int numberOfHorizontalLines = horizontalLines.size();
+            final int numberOfVerticalLines = horizontalLines.get(0).length();
+            for (int i = 0; i < numberOfVerticalLines; i++) {
+                final StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < numberOfHorizontalLines; j++) {
+                    sb.append(horizontalLines.get(j).charAt(i));
+                }
+                verticalLines.add(sb.toString());
+            }
+        }
+
+        private int getPositionFromLines(final List<String> lines) {
+            for (int i = 1; i < lines.size(); i++) {
+                if (checkIfMirror(lines, i - 1, i)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private boolean checkIfMirror(final List<String> lines,
+                                      final int backwardIndex,
+                                      final int forwardIndex) {
+            if (forwardIndex >= lines.size() || backwardIndex < 0) {
+                return true;
+            }
+            if (!lines.get(forwardIndex).equals(lines.get(backwardIndex))) {
+                return false;
+            }
+            return checkIfMirror(lines, backwardIndex - 1, forwardIndex + 1);
+        }
+    }
+}
---
+++ src/test/java/com/hodvidar/adventofcode/y2023/Day13Test.java
@@ -0,0 +1,25 @@
+package com.hodvidar.adventofcode.y2023;
+
+class Day13Test extends AbstractTestForAdventOfCode2023 {
+
+    protected Day13Test() {
+        super(new Day13());
+    }
+
+    @Override
+    protected int getNumberOfTheTest() {
+        return 1;
+    }
+
+    @Override
+    protected double getExpectedTestResultDouble() {
+        return 405d;
+    }
+
+    @Override
+    protected double getExpectedResultDouble() {
+        return 30705d;
+    }
+
+
+}
