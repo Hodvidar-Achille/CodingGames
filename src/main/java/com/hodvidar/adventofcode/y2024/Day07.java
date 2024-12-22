3c15f1e1b33d0e52055f45ed17cccd2cd6863f15
Achille Genet <achille.genet@tinubu.com>
2024-12-22 16:12:46 +0100

    AOC 2024 Day7 p1 et p2

---
+++ src/main/java/com/hodvidar/adventofcode/y2024/Day07.java
@@ -0,0 +1,54 @@
+package com.hodvidar.adventofcode.y2024;
+
+import java.util.Arrays;
+
+public class Day07 extends AbstractAdventOfCode2024 {
+
+    @Override
+    public int getDay() {
+        return 7;
+    }
+
+    @Override
+    public double getDigitFromLine(final String line) {
+        final String[] parts = line.trim().split(":");
+        final double expectedResult = Double.parseDouble(parts[0]);
+        final String[] partsTwo = parts[1].trim().split("\\s+");
+        final double[] numbers = Arrays.stream(partsTwo)
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+
+        // Check if the equation can produce the expected result
+        final boolean isValid = canProduceResult(numbers, expectedResult);
+
+        // Return the test value if valid, otherwise 0
+        return isValid ? expectedResult : 0d;
+    }
+
+    private boolean canProduceResult(final double[] numbers, final double expectedResult) {
+        return evaluate(numbers, 0, numbers[0], expectedResult);
+    }
+
+    protected boolean evaluate(final double[] numbers, final int index, final double currentResult, final double expectedResult) {
+        // Base case: if we have processed all numbers
+        if (index == numbers.length - 1) {
+            return currentResult == expectedResult;
+        }
+
+        // Try adding the next number
+        if (evaluate(numbers, index + 1, currentResult + numbers[index + 1], expectedResult)) {
+            return true;
+        }
+
+        // Try multiplying by the next number
+        if (evaluate(numbers, index + 1, currentResult * numbers[index + 1], expectedResult)) {
+            return true;
+        }
+        // No valid combination found
+        return otherEvaluation(numbers, index, (long) currentResult, expectedResult);
+    }
+
+    protected boolean otherEvaluation(final double[] numbers, final int index, final long currentResult, final double expectedResult) {
+        return false;
+    }
+}
---
+++ src/test/java/com/hodvidar/adventofcode/y2024/Day07Test.java
@@ -0,0 +1,17 @@
+package com.hodvidar.adventofcode.y2024;
+
+public class Day07Test extends AbstractTestForAdventOfCode2024 {
+
+    public Day07Test() {
+        super(new Day07());
+    }
+    @Override
+    protected double getExpectedResultDouble() {
+        return 7579994664753d;
+    }
+
+    @Override
+    protected int getExpectedTestResult() {
+        return 3749;
+    }
+}
