f4793e06bdba79c6ddd905f116fb0d83feb3e269
Hodvidar-Achille <hodvidar@protonmail.com>
2025-09-07 14:15:25 +0200

    Minor clean

--- src/main/java/com/hodvidar/codingame/puzzles/medium/NumberWithOnlyTwoDistinctDigits.java
+++ src/main/java/com/hodvidar/codingame/puzzles/medium/NumberWithOnlyTwoDistinctDigits.java
@@ -16,9 +16,9 @@ public class NumberWithOnlyTwoDistinctDigits {
 
         final var ans = findNumber(a, b, n);
 
-        System.out.println(ans);
         System.err.println("result: " + ans +
                 " is divided by 2^" + n + " ? : " + isDivisibleByPowerOfTwo(ans, n));
+        System.out.println(ans);
     }
 
     public static boolean isDivisibleByPowerOfTwo(final StringBuilder numberBuilder, final int n) {
@@ -37,30 +37,28 @@ public class NumberWithOnlyTwoDistinctDigits {
         final int odd = (a & 1) != 0 ? a : b;
         final int even = a + b - odd;
 
+        final var reversedDigits = getReversedDigits(n, even, odd);
+
+        // Reverse the collected digits to obtain the final string
+        final StringBuilder ans = new StringBuilder(n);
+        for (int i = reversedDigits.size() - 1; i >= 0; i--) {
+            ans.append(reversedDigits.get(i));
+        }
+        return ans;
+    }
+
+    private static List<Character> getReversedDigits(final int n, final int even, final int odd) {
         BigInteger k = BigInteger.ZERO;
         // digits collected in reverse order
         final List<Character> revDigits = new ArrayList<>(n);
-
         for (int i = 1; i <= n; i++) {
-            // choose the digit for the current (right‑most) position
-            // k even → even, else odd
-            final int d = (!k.testBit(0)) ? even : odd;
+            final int d = (k.testBit(0)) ? odd : even;
             revDigits.add((char) ('0' + d));
-
-            // compute 5^{i‑1}  (big‑integer power)
             final BigInteger fivePow = BigInteger.valueOf(5).pow(i - 1);
-
-            // k ← (d·5^{i‑1} + k) / 2   (integer division)
             final BigInteger term = BigInteger.valueOf(d).multiply(fivePow);
             // divide by 2 using a right shift
             k = term.add(k).shiftRight(1);
         }
-
-        // Reverse the collected digits to obtain the final string
-        final StringBuilder ans = new StringBuilder(n);
-        for (int i = revDigits.size() - 1; i >= 0; i--) {
-            ans.append(revDigits.get(i));
-        }
-        return ans;
+        return revDigits;
     }
 }
